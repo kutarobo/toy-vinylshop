e89534c94c9c95f85d66d521b5a9c298ae43643a
kutarobo <kutarobo@naver.com>
2021-06-18 04:08:33 +0900

    feat: Add preferance
    프로젝트 기본 설정 업로드

---
+++ src/main/java/com/toy/vinylshop/VinylshopApplication.java
@@ -0,0 +1,13 @@
+package com.toy.vinylshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/vinylshop/VinylshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.vinylshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
